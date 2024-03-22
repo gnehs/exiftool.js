b4e944fc36f92433812757c79a046d25d4dfb8de
gnehs <gnehs@gnehs.net>
2024-03-23 03:35:29 +0800

    added fuji tags

--- exiftool.js
+++ exiftool.js
@@ -476,6 +476,8 @@
       0x8298: "Copyright",
       0xa431: "SerialNumber", // for when SerialNumber is in IFD0 directly (BodySerialNumber in EXIF spec)
       0xa435: "LensSerialNumber", // for when LensSerialNumber is in IFD0 directly
+      // fijifilm tags
+      0x1401: "FilmMode",
     };
 
     EXIF.GPSTags = {
@@ -655,6 +657,25 @@
         4: "R",
         5: "G",
         6: "B",
+      },
+      // fijifilm tags
+      FilmMode: {
+        0x000: "F0/Standard (Provia)",
+        0x100: "F1/Studio Portrait",
+        0x110: "F1a/Studio Portrait Enhanced Saturation",
+        0x120: "F1b/Studio Portrait Smooth Skin Tone (Astia)",
+        0x130: "F1c/Studio Portrait Increased Sharpness",
+        0x200: "F2/Fujichrome (Velvia)",
+        0x300: "F3/Studio Portrait Ex",
+        0x400: "F4/Velvia",
+        0x500: "Pro Neg. Std",
+        0x501: "Pro Neg. Hi",
+        0x600: "Classic Chrome",
+        0x700: "Eterna",
+        0x800: "Classic Negative",
+        0x900: "Bleach Bypass",
+        0xa00: "Nostalgic Neg",
+        0xb00: "Reala ACE",
       },
     };
 
